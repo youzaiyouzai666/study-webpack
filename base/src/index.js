7030e522a99c247f19df61189032443d0cc1546d
youzaiyouzai666 <1620030379@qq.com>
2018-03-04 15:53:25 +0800

    step2 base css

--- base/src/index.js
+++ base/src/index.js
@@ -1,10 +1,13 @@
 import * as _ from 'lodash';
+import './style.css';
+
 // const _ = require('lodash');
 function component(){
     var element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello','webpack'],'');
-
+    element.classList.add('hello');
+    
     return element;
 }
 document.body.appendChild(component());
--- base/webpack.config.js
+++ base/webpack.config.js
@@ -2,12 +2,23 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry:'./src/index.js',
-    output:{
-        filename:'bundle.js',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     },
     plugins: [
         // new CleanWebpackPlugin(['dist'])
-      ],
+    ],
 };
