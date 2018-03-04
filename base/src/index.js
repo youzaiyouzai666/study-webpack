1a4eb208a30af4df347b8b647b899ce5120e2552
youzaiyouzai666 <1620030379@qq.com>
2018-03-04 18:26:36 +0800

    step2:回退到最基本版本

--- base/src/index.js
+++ base/src/index.js
@@ -1,19 +1,10 @@
 import * as _ from 'lodash';
-import './style.css';
-import Icon from 'Assets/img.png';
 
 // const _ = require('lodash');
 function component(){
     var element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello','webpack'],'');
-    element.classList.add('hello');
-
-       // 将图像添加到我们现有的 div。
-   var myIcon = new Image();
-   myIcon.src = Icon;
-
-   element.appendChild(myIcon);
 
     return element;
 }
--- base/webpack.config.js
+++ base/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -7,38 +6,5 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader:'css-loader',
-                        options:{
-                            alias:{
-                                './Assets': path.resolve(__dirname, 'assets/')
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    {
-                        loader:'file-loader'
-                    }
-                ]
-            }
-        ]
-    },
-    resolve:{
-        alias: {
-            Assets: path.resolve(__dirname, 'assets/')
-        }
-    },
-    plugins: [
-        // new CleanWebpackPlugin(['dist'])
-    ],
+  
 };
